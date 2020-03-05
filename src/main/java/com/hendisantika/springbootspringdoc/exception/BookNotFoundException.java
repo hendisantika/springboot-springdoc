6f18ddcd870d6875d7f894624d830a05e66a4b9c
Hendi Santika <hendisantika@gmail.com>
2020-03-06 06:44:52 +0700

    #6 - Add BookNotFoundException

---
+++ src/main/java/com/hendisantika/springbootspringdoc/exception/BookNotFoundException.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootspringdoc.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-springdoc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/03/20
+ * Time: 06.44
+ */
+@SuppressWarnings("serial")
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException() {
+
+    }
+
+}
