3bd6f3cb3efccb52db4437a12e16aef7b9f0f3ac
Hendi Santika <hendisantika@gmail.com>
2020-03-06 06:49:02 +0700

    #15 - Add updateBook in Book Controller

--- src/main/java/com/hendisantika/springbootspringdoc/controller/BookController.java
+++ src/main/java/com/hendisantika/springbootspringdoc/controller/BookController.java
@@ -4,9 +4,13 @@ import com.hendisantika.springbootspringdoc.exception.BookNotFoundException;
 import com.hendisantika.springbootspringdoc.model.Book;
 import com.hendisantika.springbootspringdoc.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
@@ -36,5 +40,11 @@ public class BookController {
     @GetMapping("/")
     public Collection<Book> findBooks() {
         return repository.getBooks();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Book updateBook(@PathVariable("id") final String id, @RequestBody final Book book) {
+        return book;
     }
 }
