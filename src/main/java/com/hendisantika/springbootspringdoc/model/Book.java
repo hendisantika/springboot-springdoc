568387ebc5c0b89189a18337bce0b7d4bf5ccd56
Hendi Santika <hendisantika@gmail.com>
2020-03-06 06:46:31 +0700

    #10 - Add Book Model

---
+++ src/main/java/com/hendisantika/springbootspringdoc/model/Book.java
@@ -0,0 +1,49 @@
+package com.hendisantika.springbootspringdoc.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-springdoc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/03/20
+ * Time: 06.46
+ */
+public class Book {
+    private long id;
+
+    @NotBlank
+    @Size(min = 0, max = 20)
+    private String title;
+
+    @NotBlank
+    @Size(min = 0, max = 30)
+    private String author;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
