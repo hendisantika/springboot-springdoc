209a2b4d46cdcb87d8fd44acc1500c2df278a994
Hendi Santika <hendisantika@gmail.com>
2020-03-06 06:48:11 +0700

    #13 - Add findById in Book Controller

--- src/main/java/com/hendisantika/springbootspringdoc/controller/BookController.java
+++ src/main/java/com/hendisantika/springbootspringdoc/controller/BookController.java
@@ -1,7 +1,11 @@
 package com.hendisantika.springbootspringdoc.controller;
 
+import com.hendisantika.springbootspringdoc.exception.BookNotFoundException;
+import com.hendisantika.springbootspringdoc.model.Book;
 import com.hendisantika.springbootspringdoc.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +24,10 @@ public class BookController {
 
     @Autowired
     private BookRepository repository;
+
+    @GetMapping("/{id}")
+    public Book findById(@PathVariable long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new BookNotFoundException());
+    }
 }
