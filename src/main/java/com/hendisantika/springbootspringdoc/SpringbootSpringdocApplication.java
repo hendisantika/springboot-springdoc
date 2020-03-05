fd36a04160da8cc7f178c40549e0c52017c03436
Hendi Santika <hendisantika@gmail.com>
2020-03-06 06:41:12 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootspringdoc/SpringbootSpringdocApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootspringdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringdocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSpringdocApplication.class, args);
+    }
+
+}
