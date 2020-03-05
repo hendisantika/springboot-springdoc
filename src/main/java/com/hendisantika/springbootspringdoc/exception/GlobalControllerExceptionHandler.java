54e7ee0cd09526845dfbe8771dcb4f531012ad97
Hendi Santika <hendisantika@gmail.com>
2020-03-06 06:45:19 +0700

    #7 - Add GlobalControllerExceptionHandler

---
+++ src/main/java/com/hendisantika/springbootspringdoc/exception/GlobalControllerExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootspringdoc.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-springdoc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/03/20
+ * Time: 06.45
+ */
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+}
